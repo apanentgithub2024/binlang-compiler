d5b176591fefd5ac42f288435d593043c1153ed3
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-10-04 19:46:52 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ var BINLang = (function(code) {
 		return new Uint8Array(id.split("").map(i => i.charCodeAt(0) - 65))
 	}
 	const reg = /(DEF|SET)\s+[a-zA-Z]*|[0-9]+/g
-	const tokens = [...code.match(reg)]
+	const tokens = code.match(reg)
 	const array = [0]
-	let id = 0, c
-	for (const token of tokens) {
+	let id = 0, c, token
+	for (let i = 0; i < tokens.length; i++) {
+		token = tokens[i]
 		switch (id) {
 			case 0:
 				c = token.slice(0, 3)
