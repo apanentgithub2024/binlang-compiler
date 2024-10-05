e69b20bf943c81fdf83ccc76835764e17a209d4e
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-10-05 12:06:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var BINLang = (function(code, o) {
 	function compileIdentifier(id) {
 		return new Uint8Array(id.split("").map(i => i.charCodeAt(0) - 65))
 	}
-	const reg = /(DEF|SET|UNL)\s+[a-zA-Z]*|[0-9]+/gm
+	const reg = /(DEF|SET|UNL)\s+|[0-9]+|[a-zA-Z]+/gm
 	const array = [0], tokens = [...code.matchAll(reg).map(i => i.join(""))]
 	let id = 0, c
 	for (const token of tokens) {
@@ -31,9 +31,7 @@ var BINLang = (function(code, o) {
 				}
 				array.push(0)
 				const tok = compileIdentifier(token)
-				array.push(...tok)
-				array.push(255)
-				array.push(0)
+				array.push(...tok, 255, 0)
 				id = 0
 				break
 			case 2:
@@ -41,11 +39,9 @@ var BINLang = (function(code, o) {
 				if (token.length === 0) {
 					throw new SyntaxError("Expected variable identifier after keyword 'SET', got empty string")
 				}
-				array.push(1)
+				array.push(0)
 				const tok2 = compileIdentifier(token)
-				array.push(...tok2)
-				array.push(255)
-				array.push(1)
+				array.push(...tok2, 255, 1)
 				id = 3
 				break
 			case 3:
@@ -64,16 +60,14 @@ var BINLang = (function(code, o) {
 				if (token.length === 0) {
 					throw new SyntaxError("Expected variable identifier after keyword 'UNL', got empty string")
 				}
-				array.push(1)
-				const tok3 = compileIdentifier(token)
-				array.push(...tok3)
-				array.push(255)
 				array.push(0)
+				const tok3 = compileIdentifier(token)
+				array.push(...tok3, 255, 0)
 				id = 0
 				break
 		}
 	}
-	c = i = "collect this garbage"
+	c = "collect this garbage"
 	const arrbuffer = new ArrayBuffer(array.length)
 	const uint = new Uint8Array(arrbuffer)
 	uint.set(array)
