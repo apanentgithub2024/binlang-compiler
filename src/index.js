dff8e993d19f0a177918974f1fb9a22b2b0f0bfe
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-10-13 18:53:04 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // PLATFORM: GitHub
 // PROFILE: https://github.com/Unnamedbruh
 // LICENSE: MIT License
-var BINLang = (function(code, o, compressed = true, interpretCheck = true) {
+var BINLang = (function(code, o, compressed = true) {
 	const idPointer = []
 	function compileIdentifier(id, unique = false) {
 		const data = new Uint8Array(id.split("").map(i => i.charCodeAt(0) - 65)), arr = []
@@ -27,7 +27,7 @@ var BINLang = (function(code, o, compressed = true, interpretCheck = true) {
 	}
 	const reg = /[0-9]+|[a-zA-Z]+/gm
 	const array = [0], tokens = [...code.matchAll(reg).map(i => i.join(""))]
-	let id = 0, c, val, vars = {}, lastVar
+	let id = 0, c, val
 	for (const token of tokens) {
 		switch (id) {
 			case 0:
@@ -57,14 +57,6 @@ var BINLang = (function(code, o, compressed = true, interpretCheck = true) {
 				const tok = compileIdentifier(token, true)
 				array.push(...tok, 255)
 				id = 0
-				if (interpretCheck) {
-					const ca = [...tok].reduce((e, i) => e + String.fromCharCode(i + 1), "")
-					if (vars[ca] === null || vars[ca] === 0 || vars[ca]) {
-						throw new ReferenceError("One of the variables are already defined")
-					} else {
-						vars[ca] = null
-					}
-				}
 				break
 			case 2:
 				c = null
@@ -74,9 +66,6 @@ var BINLang = (function(code, o, compressed = true, interpretCheck = true) {
 				array.push(0)
 				const tok2 = compileIdentifier(token)
 				array.push(...tok2, 255)
-				if (interpretCheck) {
-					lastVar = tok2
-				}
 				id = 3
 				break
 			case 3:
@@ -89,24 +78,6 @@ var BINLang = (function(code, o, compressed = true, interpretCheck = true) {
 				}
 				array.push(c)
 				id = 0
-				if (interpretCheck) {
-					const ca = [...lastVar].reduce((e, i) => e + String.fromCharCode(i + 1), "")
-					if (vars[ca] === null || vars[ca] === 0 || vars[ca]) {
-						switch (val) {
-							case "SET":
-								vars[ca] = c
-								break
-							case "ADD":
-								vars[ca] += c
-								if (vars[ca] > 255) {
-									throw new TypeError("The answer to two integers added cannot exceed beyond 255 - for more info, 8 bits are used per integer type")
-								}
-								break
-						}
-					} else {
-						throw new ReferenceError("One of the variables are not defined yet")
-					}
-				}
 				break
 			case 4:
 				c = null
@@ -116,14 +87,6 @@ var BINLang = (function(code, o, compressed = true, interpretCheck = true) {
 				array.push(0)
 				const tok3 = compileIdentifier(token)
 				array.push(...tok3, 255)
-				if (interpretCheck) {
-					const ca = [...lastVar].reduce((e, i) => e + String.fromCharCode(i + 1), "")
-					if (vars[ca] === null || vars[ca] === 0 || vars[ca]) {
-						delete vars[ca]
-					} else {
-						throw new ReferenceError("One of the variables are not defined yet")
-					}
-				}
 				id = 0
 				break
 			case 5:
